d7f7c8f0fe858b97be796dbcf6a8c33013cab12c
kp3166048-glitch <kp3166048@gmail.com>
2026-01-30 01:39:57 +0530

    Add SimpleInterest class to calculate interest

---
+++ SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        int pri = 4000;   // Principal
+        int r = 3;        // Rate of interest
+        int t = 4;        // Time in years
+        int si = (pri * r * t) / 100;  // Simple Interest formula
+        System.out.println(si);  // Correct variable name
+    }
+}
